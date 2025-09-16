f2a8f9c27e828addf5e40f5fab5a9595502a4520
Ly-sec <itslysec@gmail.com>
2025-09-16 09:52:56 +0200

    Create stripped down hyprland config hypr-noctalia: create very small config for hyprland with noctalia-greet qsgreet: add hypr-noctalia exec

--- Assets/qsgreet.sh
+++ Assets/qsgreet.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# Minimal Niri container session for Quickshell greeter
 
 # Force Qt to use Wayland
 export QT_QPA_PLATFORM=wayland
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export EGL_PLATFORM=gbm
 
-# Launch Niri with minimal config in a new VT. Use exec so greetd can kill it on login.
-exec niri -c ~/.config/quickshell/noctalia-greet/Assets/noctalia.kdl
+# Launch with minimal config in a new VT. Use exec so greetd can kill it on login.
+exec niri -c /mnt/storage/GitHub/noctalia-dev/noctalia-greet/Assets/niri-noctalia.kdl
+#exec hyprland -c /mnt/storage/GitHub/noctalia-dev/noctalia-greet/Assets/hypr-noctalia.conf
